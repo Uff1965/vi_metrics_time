ea53acee3c0b8ea57fe727fe3644759a1c8e6cd4
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-05-18 15:01:44 +0300

    bash and batch scripts

--- source/doc/vi_metrics_time.cmd
+++ source/doc/vi_metrics_time.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 ECHO START: %DATE%
+ECHO.
 
 SET cnt=1
 IF NOT %1.==. SET cnt=%1
@@ -10,11 +11,17 @@ IF NOT %2.==. SET rep=%2
 
 IF NOT %3.==. SET filter=-i %3
 
-FOR /l %%n IN (1, 1, %cnt%) DO (
+SET par=-r %rep% %filter%
+ECHO Parameters: %par%
+ECHO.
+
+FOR /L %%n IN (1, 1, %cnt%) DO (
 	TIME /t
 	ECHO Started measurement %%n of %cnt%...
-	START /B /WAIT /ABOVENORMAL .\vi_metrics_time.exe -r %rep% %filter% >vi_metrics_time.%COMPUTERNAME%.%DATE%.%%n.txt
+	START /ABOVENORMAL /WAIT /B vi_metrics_time.exe %par% >vi_metrics_time.%COMPUTERNAME%.%%n.txt
+REM	TIMEOUT /T 1
 	ECHO done.
+	ECHO.
 )
 
 ECHO FINISH: %TIME%
--- source/doc/vi_metrics_time.sh
+++ source/doc/vi_metrics_time.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
-# Comment )
 
-echo START: $(date)
+echo START: $(date '+%Y-%m-%d')
+echo
 
 program_name=vi_metrics_time
 
-number_of_times=$1
-if [ -z "$1" ]
+cnt=1
+if [ "$1" ]
 then
-	number_of_times=$1
+	cnt=$1
 fi
 
-for i in $(seq 1 $number_of_times)
+rep=20
+if [ "$2" ]
+then
+	rep=$2
+fi
+
+if [ "$3" ]
+then
+	filter="-i $3"
+fi
+
+par="-r $rep $filter"
+echo Parameters: $par
+echo
+
+for i in $(seq 1 $cnt)
 do
-	echo $(date)
-	echo Started measurement $i of $number_of_times...
-	./$program_name >$program_name.$i.txt
+	echo $(date '+%H:%M:%S'): Started measurement $i of $cnt...
+	sudo nice -n -5 ./$program_name $par >$program_name.$HOSTNAME.$i.txt
+#	sleep 1
 	echo done.
+	echo
 done
 
-echo FINISH: $(date)
+echo FINISH: $(date '+%Y-%m-%d %H:%M:%S')
