2a2429038daeb09c5daf12076f65a6c092c1a92c
Ilya Tikhomirov <Uff1965@rambler.ru>
2024-05-18 13:16:25 +0300

    Step

--- source/doc/vi_metrics_time.sh
+++ source/doc/vi_metrics_time.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 # Comment )
 
+echo START: $(date)
+
+program_name=vi_metrics_time
+
+number_of_times=$1
+if [ -z "$1" ]
+then
+	number_of_times=$1
+fi
+
+for i in $(seq 1 $number_of_times)
+do
+	echo $(date)
+	echo Started measurement $i of $number_of_times...
+	./$program_name >$program_name.$i.txt
+	echo done.
+done
+
+echo FINISH: $(date)
